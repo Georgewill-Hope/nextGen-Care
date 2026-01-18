759720ae7e7b64207d17a96c712dc3d16a926146
Georgewill Hope <georgeblue222@gmail.com>
2026-01-18 23:35:06 +0100

    Developed mobile navbar

---
+++ app/get-involved/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const getInvolved = () => {
+  return (
+    <div>getInvolved</div>
+  )
+}
+
+export default getInvolved
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import logo from "@/public/assets/logo2.png";
@@ -8,11 +8,15 @@ import { FaFacebook } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { IoCloseOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  const path = usePathname()
+  const path = usePathname();
+  const [openNav, setOpenNav] = useState(false);
   return (
-    <nav className="bg-foreground fixed left-0 right-0 z-50 top-0 px-5">
+    <nav className="lg:bg-foreground fixed left-0 right-0 z-50 top-0 lg:px-5">
       {/* Desktop and large device navbar */}
 
       <div className="hidden lg:flex">
@@ -22,15 +26,19 @@ const Navbar = () => {
             <Image src={logo} alt="NextGen Care logo" width={120} height={50} />
           </div>
 
-          <ul className="perfectCenter gap-10">
+          <ul className="perfectCenter xl:gap-10">
             {navigationData.map((item) => (
-              <li
-                key={item.id}
-                className={`text-background font-semibold tracking-wider font-lato p-5 ${
-                  path === item.href && "border-b-4 border-custom-orange"
-                }`}
-              >
-                <Link href={item.href}>{item.page}</Link>
+              <li key={item.id}>
+                <Link
+                  href={item.href}
+                  className={`text-background font-semibold tracking-wider font-lato p-5 hover:border-b-4 hover:border-custom-orange transition-all duration-500 border-b-4 ${
+                    path === item.href
+                      ? " border-custom-orange"
+                      : "border-transparent"
+                  }`}
+                >
+                  {item.page}
+                </Link>
               </li>
             ))}
           </ul>
@@ -38,13 +46,21 @@ const Navbar = () => {
           {/* Social Links */}
 
           <div className="perfectCenter gap-5">
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/share/1Bu31bWk4E/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FaFacebook
                 size={25}
                 className="text-background dark:text-background"
               />
             </a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://x.com/Hopedev_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BsTwitterX
                 size={22}
                 className="text-background dark:text-background"
@@ -61,7 +77,83 @@ const Navbar = () => {
       </div>
 
       {/* mobile phones and small device navbar */}
-      <div></div>
+      <div className="flex lg:hidden w-full">
+        <div className="w-full">
+          <div className="w-full bg-foreground z-999 relative">
+            <div className="items-center-between px-5 py-2 container-width">
+              {/* Logo */}
+              <Image
+                src={logo}
+                alt="NextGen Care logo"
+                width={100}
+                height={50}
+              />
+
+              <button onClick={() => setOpenNav(!openNav)}>
+                {openNav ? (
+                  <IoCloseOutline color="white" size={30} />
+                ) : (
+                  <HiOutlineBars3 color="white" size={30} />
+                )}
+              </button>
+            </div>
+          </div>
+
+          <div
+            className={`relative h-screen bg-foreground transition-transform duration-500 ease-in-out ${
+              openNav ? "translate-y-0" : "-translate-y-300"
+            }`}
+          >
+            {/* nav links */}
+            <ul className="perfectCenter flex-col gap-10 py-10 mb-10">
+              {navigationData.map((item) => (
+                <li key={item.id}>
+                  <Link
+                    href={item.href}
+                    className={`text-background font-semibold tracking-wider font-lato p-5 hover:border-b-4 hover:border-custom-orange transition-all duration-500 border-b-4 ${
+                      path === item.href
+                        ? " border-custom-orange"
+                        : "border-transparent"
+                    }`}
+                  >
+                    {item.page}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+
+            {/* Social Links */}
+            <div className="perfectCenter gap-5">
+              <a
+                href="https://www.facebook.com/share/1Bu31bWk4E/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebook
+                  size={25}
+                  className="text-background dark:text-background"
+                />
+              </a>
+              <a
+                href="https://x.com/Hopedev_"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BsTwitterX
+                  size={22}
+                  className="text-background dark:text-background"
+                />
+              </a>
+              <a href="http://" target="_blank" rel="noopener noreferrer">
+                <AiOutlineYoutube
+                  size={30}
+                  className="text-background dark:text-background"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
