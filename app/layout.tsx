4aa8692bbe266d3f82dde2f33806be7479efb2ef
Georgewill Hope <georgeblue222@gmail.com>
2026-01-19 17:54:37 +0100

    I changed open graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,34 +34,6 @@ export const metadata: Metadata = {
     "Philippines",
   ],
   authors: [{ name: "Next Generation Care" }],
-  metadataBase: new URL("https://next-gen-care-hopedev.vercel.app"),
-  openGraph: {
-    type: "website",
-    url: "https://next-gen-care-hopedev.vercel.app/",
-    title:
-      "Home | Next Generation Care | Orphanage in Mauritania | Philippines | Africa",
-    description:
-      "We offer orphanages, adoption centers, and child learning services to parentless children in the Philippines, Africa and Mauritania.",
-    siteName: "Next Generation Care",
-    images: [
-      {
-        url: "/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Next Generation Care",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@Hopedev_",
-    creator: "@Hopedev_",
-    title:
-      "Home | Next Generation Care | Orphanage in Mauritania | Philippines | Africa",
-    description:
-      "We offer orphanages, adoption centers, and child learning services to parentless children in the Philippines, Africa and Mauritania.",
-    images: ["/twitter-image.png"],
-  },
 };
 
 export default function RootLayout({
