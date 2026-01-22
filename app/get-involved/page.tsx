308738507266cdae39a2fc494807d2c216679415
Georgewill Hope <georgeblue222@gmail.com>
2026-01-22 22:51:39 +0100

    I developed the get-involved page and made it fully responsive

--- app/get-involved/page.tsx
+++ app/get-involved/page.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import Button from "@/components/Button";
+import Estimate from "@/components/Estimate";
+import GetInvolved from "@/components/GetInvolved";
+import Help from "@/components/Help";
+import Hero from "@/components/Hero";
+import bg from "@/public/assets/pexels-safari-consoler-3290243-14456845.jpg";
+import Image from "next/image";
+import { FaMoneyCheckAlt } from "react-icons/fa";
+import { GiPayMoney, GiReceiveMoney, GiTakeMyMoney } from "react-icons/gi";
+import { MdVolunteerActivism } from "react-icons/md";
+import {
+  RiMoneyCnyBoxFill,
+  RiMoneyDollarBoxFill,
+  RiMoneyPoundCircleFill,
+} from "react-icons/ri";
 
 const getInvolved = () => {
   return (
-    <div>getInvolved</div>
-  )
-}
+    <div>
+      <Hero image={bg} imageStyle="grayscale-50">
+        <div className="flex flex-col items-start justify-center gap-5 container-width self-end pb-5">
+          {/* Content Description */}
+          <p className="text-white text-sm sm:text-lg tracking-widest font-mono text-shadow">
+            Ways to Support
+          </p>
 
-export default getInvolved
+          {/* Content Header */}
+          <h1 className="text-white text-5xl lg:text-7xl font-semibold font-lato">
+            Let&apos;s Spread Smile Not Tears
+            <br /> Come Cultivate Love Not Fears
+          </h1>
+        </div>
+      </Hero>
+
+      <Estimate
+        title="“It takes a Society to raise a child”"
+        titleStyles="text-start self-start border-b border-custom-orange"
+      >
+        <p className="self-start">
+          This little NGO is steadily growing to cover the whole world. We work
+          hard to ensure that no child sleeps without a roof above their head or
+          a warm embrace to provide comfort and security.
+        </p>
+
+        <p className="self-start">
+          The organization has also taken the initiative to start educating
+          these children for a better future.
+        </p>
+
+        <p className="self-start">
+          But we can&apos;t possibly do this without you. Your tiniest support
+          can go a long way to making their big dreams come true.
+        </p>
+      </Estimate>
+      <GetInvolved />
+      <Estimate title="Our promise" titleStyles="border-b border-custom-orange">
+        <p className="text-center font-lato">
+          We assure you that all your donations will reach these children and be
+          invested in their future.
+        </p>
+
+        <p className="text-center font-lato">
+          If a single drop of water can start a ripple effect in the ocean then
+          one penny from your end can also leave a lasting impression <br /> on
+          these lives.{" "}
+          <span className="text-lg font-mono font-semibold tracking-widest">
+            Every Penny Counts
+          </span>
+        </p>
+
+        <p className="text-center font-mono font-semibold tracking-widest">
+          Make Your Contribution for Their Future.
+        </p>
+
+        <div className="flex items-end justify-center md:justify-between flex-wrap gap-10 w-full mt-10">
+          <div className="flex flex-col items-center justify-center">
+            <MdVolunteerActivism size={50} className="text-custom-orange" />
+
+            <h4 className="p-5 text-center font-bold font-lato">Volunteer</h4>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+              <GiPayMoney size={50} className="text-yellow-400 ml-6" />
+              <GiReceiveMoney size={50} className="text-yellow-400 mr-5" />
+            </div>
+
+            <h4 className="p-5 text-center font-bold font-lato">
+              Giving Wednesdays
+            </h4>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <GiTakeMyMoney size={50} className="text-green-600" />
+
+            <h4 className="p-5 text-center font-bold font-lato">Fundraise</h4>
+          </div>
+        </div>
+
+        <div className="mt-14">
+          <Button title="Donate Now" href="&#" />
+        </div>
+      </Estimate>
+      <Help />
+    </div>
+  );
+};
+
+export default getInvolved;
---
+++ components/GetInvolved.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import bg from "@/public/assets/logo2.png";
+import ebay from "@/public/assets/ebay-charity-logo-600x450-1-removebg-preview.png";
+import stripe from "@/public/assets/stripe2.png";
+import paypal from "@/public/assets/paypal-donate2-removebg-preview.png";
+import Gpay from "@/public/assets/apple-pay.png";
+import { FaCcStripe } from "react-icons/fa";
+
+const GetInvolved = () => {
+  return (
+    <section className="bg-gray-100 dark:bg-black">
+      <div className="container-width flex items-center flex-col gap-5 py-20">
+        <h3 className="text-2xl sm:text-4xl mb-5 sm:mb-10 pb-2 border-b border-custom-orange">
+          How Can You Get involved?
+        </h3>
+
+        <div className="perfectCenter flex-col gap-3 md:gap-0 sm:tracking-wider mb-5 sm:mb-10 md:leading-8 font-lato text-center">
+          <p>Plenty if you set your heart to it!</p>
+          <p>
+            There are multiple ways to send in your charitable donations our
+            way. Pick the one that suits you:
+          </p>
+          <p>
+            <b>Come participate in our online funding programs:</b>
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center lg:justify-between flex-wrap gap-10 w-full">
+          <div className="cursor-pointer w-full sm:w-fit">
+            <div className="relative w-full sm:w-64 h-24 overflow-hidden">
+              <Image
+                src={ebay}
+                alt="ebay image"
+                fill
+                className="object-center object-contain"
+              />
+            </div>
+            <h4 className="text-center font-extrabold font-lato">eBay</h4>
+          </div>
+
+          <div className="cursor-pointer w-full sm:w-fit">
+            <div className="relative w-full sm:w-64 h-24 overflow-hidden">
+              <Image
+                src={stripe}
+                alt="ebay image"
+                fill
+                className="object-center object-contain"
+              />
+            </div>
+            <h4 className="text-center font-extrabold font-lato">Stripe</h4>
+          </div>
+
+          <div className="cursor-pointer w-full sm:w-fit">
+            <div className="relative w-full sm:w-64 h-24 overflow-hidden">
+              <Image
+                src={paypal}
+                alt="ebay image"
+                fill
+                className="object-center object-contain"
+              />
+            </div>
+            <h4 className="text-center font-extrabold font-lato">PayPal</h4>
+          </div>
+          <div className="cursor-pointer w-full sm:w-fit">
+            <div className="relative w-full sm:w-64 h-24 overflow-hidden">
+              <Image
+                src={Gpay}
+                alt="ebay image"
+                fill
+                className="object-center object-contain"
+              />
+            </div>
+            <h4 className="text-center font-extrabold font-lato">GPay</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetInvolved;
