5807907ddaaf4d4270f870f03a223159b3541f3c
Georgewill Hope <georgeblue222@gmail.com>
2026-01-23 23:40:38 +0100

    I developed Programs page hero section

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Hero from "@/components/Hero";
+import bg from "@/public/assets/hossen.jpg";
 
 const programs = () => {
   return (
-    <div>programs</div>
-  )
-}
+    <div>
+      <Hero image={bg} imageStyle="grayscale-50">
+        <div className="flex items-center justify-center container-width">
+          {/* Content Header */}
+          <h1 className="text-white text-center text-5xl lg:text-7xl font-semibold font-lato">
+            Orphan Lifestyle Programs
+          </h1>
+        </div>
+      </Hero>
+    </div>
+  );
+};
 
-export default programs
+export default programs;
